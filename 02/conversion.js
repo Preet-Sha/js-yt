7a3e998e54ac97de48aefc4c0cfb9b49f121398f
Preet-Sha <preetspersonal@gmail.com>
2025-03-24 09:20:55 +0000

    conversion data types

---
+++ 02/conversion.js
@@ -0,0 +1,20 @@
+let score ="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let val=Number(score)
+console.log(typeof val);
+console.log(val);
+ // "22"=> 22
+ // "33qbc" =>  NaN
+ // true => 1 ; false => 0
+
+
+ //1 => true (in Boolean)
+ // ""=>false
+ // "preet" => true
+
+ let some=33
+ let string = String(some);
+console.log(string);
+console.log(typeof string);
