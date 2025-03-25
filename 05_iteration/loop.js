cbdeff00bb8c386174900825104cb77b05babee6
Preet-Sha <preetspersonal@gmail.com>
2025-03-25 08:30:40 +0000

    loop

---
+++ 05_iteration/loop.js
@@ -0,0 +1,39 @@
+//for loop 
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element);
+    
+    
+}
+
+//while loop 
+let index=0
+while (index<10) {
+    console.log(`value of index is ${index}`);
+    index+=2
+    
+    
+}
+let myarr=['superman','batman','flash']
+let i=0;
+while(i<myarr.length){
+    console.log(`hero name is ${myarr[i]}`);
+    i+=1
+    
+}
+
+
+//do-while loop
+
+
+let score=11
+do {
+    console.log(`score is ${score}`); //pehle chalega hi chalega 
+    score++
+    
+} while (score<=10);
+
+
+//array par loop lagana specialised loop 
+// in upcoming video
